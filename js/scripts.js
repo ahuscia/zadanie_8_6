e4365aa6294f9bafa88130d8fdafdcd3f89e4464
ahuscia <a.huscia@gmail.com>
2018-02-06 15:50:01 +0100

    Add index.html and script.js

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// scripts.js
+
+var a = prompt("First number: ");
+var b = prompt("Second number: ");
+var value = (a * a) - (2 * a * b) - (b * b);
+
+
+if ( value < 0 ) {
+    alert('wynik ujemny');
+    console.log('wynik ujemny');
+} else if ( value > 0 ) {
+    alert('wynik ujemny');
+    console.log('wynik dodatni');
+} else if ( value == 0 ) {
+    alert('wynik równy 0');
+    console.log('wynik równy 0');
+}
